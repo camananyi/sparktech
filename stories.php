1548b5d27a66d3c63a6e70743e86907809d3c6af
camananyi <62218927+camananyi@users.noreply.github.com>
2026-06-18 16:58:10 +0000

    Model.php6

--- stories.php
+++ stories.php
@@ -38,7 +38,7 @@
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
         <li class="nav-item"><a class="nav-link" href="model.php">Model</a></li>
-        <li class="nav-item"><a class="nav-link" href="register.php">Register</a></li>
+        <li class="nav-item"><a class="nav-link" href="https://docs.google.com/forms/d/e/1FAIpQLSeKdf_P5CB-lE-levyxtetAd2zsGTqXbzfwXf4rfQ6CDhQrCA/viewform?usp=header">Register</a></li>
         <!-- <li class="nav-item"><a class="nav-link" href="login.php">Login</a></li> -->
         <li class="nav-item"><a class="nav-link" href="https://albums.memento.com/sparktechafrica">Gallery</a></li>
         <li class="nav-item"><a class="nav-link" href="learn.php">Learn</a></li>
